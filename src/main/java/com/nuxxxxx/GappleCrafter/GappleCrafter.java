3662b8e2d26ef79e7c52b453c24edee4108ad7cf
Nux <59640444+nuxxxxx@users.noreply.github.com>
2025-02-20 17:48:57 +0100

    Update GappleCrafter.java

--- src/main/java/com/nuxxxxx/GappleCrafter/GappleCrafter.java
+++ src/main/java/com/nuxxxxx/GappleCrafter/GappleCrafter.java
@@ -4,15 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GappleCrafter extends JavaPlugin {
 
-@Override
-public void onEnable() {
-    getLogger().info("GappleCrafter has been enabled!");
-    saveDefaultConfig();  // Ensure the default config exists
-    RecipeManager.setup(this);  // Load recipes and effects from the config
-    getServer().getPluginManager().registerEvents(new ItemEffectListener(), this);  // Register the listener
-}
-
-
+    @Override
+    public void onEnable() {
+        getLogger().info("GappleCrafter has been enabled!");
+        saveDefaultConfig();  // Ensure the default config exists
+        RecipeManager.setup(this);  // Load recipes and effects from the config
+        getServer().getPluginManager().registerEvents(new ItemEffectListener(this), this);  // Pass the plugin instance to the listener
+    }
 
     @Override
     public void onDisable() {
