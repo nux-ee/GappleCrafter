6ea405fff5b394c94b5f31ca9051671b696dc04e
Nux <59640444+nux-ee@users.noreply.github.com>
2025-02-20 08:41:08 +0100

    Create RecipeManager.java

---
+++ src/main/java/com/nux-ee/GappleCrafter/RecipeManager.java
@@ -0,0 +1,19 @@
+package com.yourname.gapplecrafter;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RecipeManager {
+    public static void addRecipes(JavaPlugin plugin) {
+        ItemStack goldenApple = new ItemStack(Material.GOLDEN_APPLE);
+        
+        ShapedRecipe recipe = new ShapedRecipe(goldenApple);
+        recipe.shape("GGG", "GAG", "GGG");
+        recipe.setIngredient('G', Material.GOLD_INGOT);
+        recipe.setIngredient('A', Material.APPLE);
+        
+        plugin.getServer().addRecipe(recipe);
+    }
+}
