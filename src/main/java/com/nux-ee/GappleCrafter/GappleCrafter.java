4f11578ad6bb9fe83a262d266b9e828a42e8b44b
Nux <59640444+nux-ee@users.noreply.github.com>
2025-02-20 08:40:03 +0100

    Update GappleCrafter.java

--- src/main/java/com/nux-ee/GappleCrafter/GappleCrafter.java
+++ src/main/java/com/nux-ee/GappleCrafter/GappleCrafter.java
@@ -1,1 +1,17 @@
-src/main/java/com/yourname/gapplecrafter/GappleCrafter.java
+package com.yourname.gapplecrafter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GappleCrafter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("GappleCrafter has been enabled!");
+        RecipeManager.addRecipes(this);  // Add custom crafting recipes
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("GappleCrafter has been disabled.");
+    }
+}
