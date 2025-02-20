cc578dcf3c61afe08494d47ffba88c08145ee529
Nux <59640444+nux-ee@users.noreply.github.com>
2025-02-20 13:47:59 +0100

    Update RecipeManager.java

--- src/main/java/com/nux-ee/GappleCrafter/RecipeManager.java
+++ src/main/java/com/nux-ee/GappleCrafter/RecipeManager.java
@@ -1,18 +1,23 @@
 package com.nux.ee.GappleCrafter;
+
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RecipeManager {
+
     public static void addRecipes(JavaPlugin plugin) {
-        ItemStack goldenApple = new ItemStack(Material.GOLDEN_APPLE);
+        // Enchanted Golden Apple (Notch Apple) Recipe
+        ItemStack enchantedGoldenApple = new ItemStack(Material.GOLDEN_APPLE, 1, (short) 1); // 1 refers to the Enchanted variant
         
-        ShapedRecipe recipe = new ShapedRecipe(goldenApple);
+        // Create recipe for the vanilla Enchanted Golden Apple (Notch Apple)
+        ShapedRecipe recipe = new ShapedRecipe(enchantedGoldenApple);
         recipe.shape("GGG", "GAG", "GGG");
-        recipe.setIngredient('G', Material.GOLD_INGOT);
+        recipe.setIngredient('G', Material.GOLD_BLOCK);
         recipe.setIngredient('A', Material.APPLE);
-        
+
         plugin.getServer().addRecipe(recipe);
     }
 }
